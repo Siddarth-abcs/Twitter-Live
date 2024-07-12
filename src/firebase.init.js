d66099e43b0c78894dfe6f25e381e87457c87e38
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-07-12 12:41:21 +0530

    Update firebase.init.js

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,14 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+wrong config info
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAetoZgZOyBPhscDyudteIZKnvkuuDi3Js",
-  authDomain: "github-websi.firebaseapp.com",
-  projectId: "github-websi",
-  storageBucket: "github-websi.appspot.com",
-  messagingSenderId: "1023424542464",
-  appId: "1:1023424542464:web:5de70055b532e55e2325e5",
-  measurementId: "G-CNGW915LJ8",
+  apiKey: "AIzteIZKnvkuuDi3Js",
+  authDomain: "github-.fipp.com",
+  projectId: "github-si",
+  storageBucket: "github-wi.appspot.com",
+  messagingSenderId: "102,
+  appId: "1:1023424542464:wb532e55e2325e5",
+  measurementId: "G",
 };
 
 // Initialize Firebase
